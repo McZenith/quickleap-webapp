298718347fa4cd3191420aa242cc55e158bb09fc
Isaiah Awotide <awotideisaiah@yahoo.com>
2020-05-11 15:04:56 +0100

    Added Normalise.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import App from "next/app";
+import Head from "next/head";
 import { Grommet, grommet as grommetTheme, extendDefaultTheme } from "grommet";
 
 const customTheme = {
@@ -11,6 +12,20 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props;
     return (
       <Grommet theme={customTheme} full>
+        <Head>
+          {/* <title>My page</title> */}
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+            integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU="
+            crossOrigin="anonymous"
+          />
+        </Head>
         <Component {...pageProps} />
       </Grommet>
     );
