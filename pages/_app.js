ad909d74d01061906b20c76051dd08eb07ad821f
Isaiah Awotide <awotideisaiah@yahoo.com>
2020-05-11 14:49:37 +0100

    Setup desktop nav

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import React from "react";
 import App from "next/app";
-import { Grommet, grommet as grommetTheme } from "grommet";
+import { Grommet, grommet as grommetTheme, extendDefaultTheme } from "grommet";
+
+const customTheme = {
+  global: {},
+};
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <Grommet theme={grommetTheme} full>
+      <Grommet theme={customTheme} full>
         <Component {...pageProps} />
       </Grommet>
     );
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import {
   Main,
   Box,
@@ -8,17 +9,51 @@ import {
   Footer,
   Text,
   Anchor,
+  Image,
+  ResponsiveContext,
 } from "grommet";
+const NavLinks = [
+  { title: "Home" },
+  { title: "About" },
+  { title: "Marketplace" },
+  { title: "Inventory Manager" },
+  { title: "Aggregator" },
+  { title: "Login/Sign Up" },
+  { title: "Blog" },
+];
 
-export default () => (
-  <Box>
-    <Header background="accent-1" fill="horizontal" height="10vh">
-      <Menu label="account" items={[{ label: "logout" }]} />
-      <Menu label="account" items={[{ label: "logout" }]} />
-    </Header>
-    <Main background="brand">
-      <Box background="brand" height="80vh" fill="horizontal"></Box>
-    </Main>
-    <Footer background="accent-3" height="10vh"></Footer>
-  </Box>
-);
+const Home = () => {
+  const size = React.useContext(ResponsiveContext);
+  const { link } = styles;
+  return (
+    <Box>
+      <Header fill="horizontal" height="9vh" pad="medium" elevation="medium">
+        <Box width="100px">
+          <Image fill src={"./logo.png"} alt="quickleap logo" />
+        </Box>
+        <Box direction="row" gap="medium">
+          {size === "small"
+            ? <div></div>
+            : NavLinks.map(({ title }, index) => (
+                <Link href={`/${title}`} key={index}>
+                  <a style={link}>{title}</a>
+                </Link>
+              ))}
+        </Box>
+      </Header>
+      <Main background="brand">
+        <Box height="80vh" fill="horizontal"></Box>
+      </Main>
+      <Footer background="accent-1" height="11vh"></Footer>
+    </Box>
+  );
+};
+
+const styles = {
+  link: {
+    textDecoration: "none",
+    color: "black",
+  },
+};
+
+export default Home;
